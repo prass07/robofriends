e5b43e040e9b91a0b1fc3d2e2dbe99e882ddfa22
prass07 <prasup07@gmail.com>
2020-04-25 22:30:41 +0530

    Publish the app to Github

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Container/App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ReactDOM.render(<App />
+	,
   document.getElementById('root')
 );
 
